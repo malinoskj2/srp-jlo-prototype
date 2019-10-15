70eba7848d010c6f0dd48b96d759a6bd94d5f471
Jesse Malinosky <jesse@malinoskj2.dev>
2019-10-15 12:50:44 -0400

    add audio recording

--- src/views/Exam.vue
+++ src/views/Exam.vue
@@ -1,16 +1,64 @@
 <template>
-<div>
-    <h1>This is an exam</h1>
+<div id="exam">
+    <va-button @click="startRecord" string="primary" size="md" >
+        <div>Start Recording</div>
+    </va-button>
+    <va-button @click="stopRecord" string="primary" size="md" >
+        <div>Stop Recording</div>
+    </va-button>
 </div>
 </template>
 
 <script>
     export default {
-        name: "Exam"
+        name: "Exam",
+        data () {
+            return {
+                recorder: null,
+                audio: []
+            }
+        },
+        mounted() {
+            this.initRecorder();
+        },
+        methods: {
+            initRecorder() {
+
+                const mediaAvailable = stream => {
+                    console.log('Obtained audio stream')
+                    this.recorder = new MediaRecorder(stream, { mimeType: 'audio/ogg'})
+                    this.recorder.ondataavailable = ((e) => this.audio.push(e.data))
+                };
+
+                const mediaUnavailable = error => {
+                  console.log(`Failed to obtain audio stream: ${error}`)
+                };
+
+                navigator.mediaDevices.getUserMedia({audio:true})
+                    .then(mediaAvailable, mediaUnavailable);
+
+            },
+            startRecord () {
+                console.log('Recording Audio.');
+                this.recorder.start()
+            },
+            stopRecord () {
+                console.log('Stopped Recording.');
+                this.recorder.stop();
+                console.log(this.audio);
+            }
+        }
     }
 </script>
 
 <style scoped>
+    #exam {
+        margin-top: 240px;
+        display: flex;
+        flex-direction: row;
+        justify-content: center;
+    }
+
     h1 {
         font-size: 128px;
     }
